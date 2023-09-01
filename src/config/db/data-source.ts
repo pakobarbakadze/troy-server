667d06abe599d0deb7697227a9d70163ec8e3991
pakobarbakadze <pakobarbakadze@gmail.com>
2023-09-01 14:21:56 +0400

    setup: typeorm

---
+++ src/config/db/data-source.ts
@@ -0,0 +1,31 @@
+import { DataSource } from "typeorm";
+import { Stay } from "../../entities/Stay";
+
+import logger from "../../utils/winston";
+
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+
+export const dataSource = new DataSource({
+  type: "postgres",
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [Stay],
+  subscribers: [],
+  migrations: [],
+});
+
+const db = async () => {
+  try {
+    await dataSource.initialize();
+    logger.info("Connection to database has been initialized");
+  } catch (err) {
+    logger.error("Error while connecting to database : " + err);
+  }
+};
+
+export default db;
